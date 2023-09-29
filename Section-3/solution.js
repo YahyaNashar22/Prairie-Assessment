dee33a228ef0d7374c1b8d6b780f1556d173f2fb
YahyaNashar22 <cenations.people@live.com>
2023-09-29 12:38:37 +0300

    starting step 3

---
+++ Section-3/solution.js
@@ -0,0 +1,9 @@
+let user_input = parseInt(prompt("enter a number"));
+function findPrimeNumbers(num) {
+  if (num <= 2) {
+    alert("choose a bigger number");
+  } else {
+    return "hello";
+  }
+}
+findPrimeNumbers(user_input);
